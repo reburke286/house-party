b305172578b3aac92a962e9606d644a8f96b58f3
reburke286 <reburke286@gmail.com>
2022-04-20 22:49:44 -0400

    put in emailjs

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    console.log('hi')
-
+    console.log("hey cutie ... :) ")
     //Modal Functions
     $("#infoModalBtn").on("click", function () {
         $("#infoModal").modal("show");
@@ -34,65 +33,6 @@ $(document).ready(function () {
         $("#rsvpModal").modal("hide")
         $("#faqModal").modal("hide")
     })
-
-    //RSVP functions
-    $(".rsvp-btn").on('click', handleRSVP)
-
-    function handleRSVP(e) {
-        e.preventDefault();
-        const name = $("#name").val()
-        const groupData = $("#group").val()
-        const yes = $("#yes-rsvp").is(':checked')
-        const no = $("#no-rsvp").is(':checked')
-
-        let answer;
-
-        if (name === "") {
-            alert("You need to tell us who you are. That seems obvious.")
-            return;
-        }
-        if (yes && no) {
-            alert("Oh, um, just pick one?")
-            return;
-        } else if (no) {
-            answer = false
-        } else if (yes) {
-            answer = true
-        }
-
-        const newRSVP = {
-            name: name,
-            group: groupData,
-            answer: answer
-        }
-
-        saveRSVP(newRSVP).then(() => {
-            $("#name").val("")
-            $("#group").val("")
-            $('#yes-rsvp').prop('checked', false);
-            $('#no-rsvp').prop('checked', false);
-            $("#rsvpModal").modal("hide");
-            alert("Congratulations! You've just done the very noble thing of telling two people in the midst of planning a huge party whether or not you're coming. We love you.")
-            getRSVPs();
-        })
-    }
-
-    const getRSVPs = () =>
-        fetch('/api/rsvps', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-    const saveRSVP = rsvp =>
-        fetch('/api/rsvps', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(rsvp)
-        })
 
 
 
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 // Initialize the app and create a port
@@ -10,7 +9,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 // Start the server on the port
